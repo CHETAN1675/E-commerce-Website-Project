6983ca5cc7a74c4329d3d1d8767b4f3293b96154
CHETAN1675 <mahajanchetan001@gmail.com>
2025-10-03 20:13:29 +0530

    Implement the Home page

--- ecommerce-website/src/components/routers/routers.jsx
+++ ecommerce-website/src/components/routers/routers.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import ProductList from "../Products/ProductList";
 import About from "../Layout/About";
-
+import Home from "../Layout/Home"
 
 const routePath = {
   Home: "/",
@@ -13,7 +13,7 @@ const Routers = () => {
   return (
     <Routes>
     
-      <Route path={routePath.Home} element={<ProductList />} />
+      <Route path={routePath.Home} element={<Home/>} />
       <Route path={routePath.Store} element={<ProductList />} />
       <Route path={routePath.About} element={<About />} />
     </Routes>
